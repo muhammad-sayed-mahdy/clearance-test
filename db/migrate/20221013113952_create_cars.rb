e18e437921c93d28ac6e7c88dc70832843994793
Muhammad Sayed <muhammad.s.mahdy@gmail.com>
2022-10-13 13:40:40 +0200

    add cars scaffold

---
+++ db/migrate/20221013113952_create_cars.rb
@@ -0,0 +1,11 @@
+class CreateCars < ActiveRecord::Migration[6.0]
+  def change
+    create_table :cars do |t|
+      t.string :model
+      t.string :make
+      t.integer :year
+
+      t.timestamps
+    end
+  end
+end
