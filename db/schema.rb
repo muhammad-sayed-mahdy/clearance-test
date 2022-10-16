4e99ef147fb3dd4bc0ead8e4f019a388e7525c1a
Muhammad Sayed <muhammad.s.mahdy@gmail.com>
2022-10-16 16:01:38 +0200

    Remove password from users
    it shouldn't be a database column, when creating a user clearance makes the user model accepts `password` parameter and then sets the `encyrpted_password` and the `remember_token`

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_13_164338) do
+ActiveRecord::Schema.define(version: 2022_10_16_133246) do
 
   create_table "cars", force: :cascade do |t|
     t.string "model"
@@ -22,7 +22,6 @@ ActiveRecord::Schema.define(version: 2022_10_13_164338) do
 
   create_table "users", force: :cascade do |t|
     t.string "email"
-    t.string "password"
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
