c0a8ba42887740b366c35302ea6a034507ef077b
Muhammad Sayed <muhammad.s.mahdy@gmail.com>
2022-10-16 13:19:53 +0200

    add users table and clearance

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_13_113952) do
+ActiveRecord::Schema.define(version: 2022_10_13_164338) do
 
   create_table "cars", force: :cascade do |t|
     t.string "model"
@@ -18,6 +18,20 @@ ActiveRecord::Schema.define(version: 2022_10_13_113952) do
     t.integer "year"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password"
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "encrypted_password", limit: 128
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token", unique: true
   end
 
 end
